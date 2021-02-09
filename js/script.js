192d9869a85404aaaf33af5ae8a1c2b3847cae84
Benjamin Malchert <b.malchert0@gmail.com>
2021-02-08 17:00:44 -0800

    Put 16 boxes in 4x4 flexbox

--- js/script.js
+++ js/script.js
@@ -4,8 +4,10 @@ console.log(container);
 // Create 16 `div` squares
 for (let i = 0; i < 16; i++) {
   const square = document.createElement('div');
-  square.style.width  = '100px';
-  square.style.height = '100px';
+  square.style.width  = '98px';
+  square.style.height = '98px';
   square.textContent  = 'Square ' + i;
+  square.className = 'square'
+  square.id = 'square' + i;
   container.appendChild(square);
 }
