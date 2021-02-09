6fe5ace5c48dd8444e38e28d7a3ffd3d3d9f1c9e
Benjamin Malchert <b.malchert0@gmail.com>
2021-02-08 16:38:06 -0800

    Add loop to create 16 div squares in #container

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
-const squares = document.querySelectorAll("#squares");
-console.log(squares);
+const container = document.querySelector('#container');
+console.log(container);
+
+// Create 16 `div` squares
+for (let i = 0; i < 16; i++) {
+  const square = document.createElement('div');
+  square.style.width  = '100px';
+  square.style.height = '100px';
+  square.textContent  = 'Square ' + i;
+  container.appendChild(square);
+}
