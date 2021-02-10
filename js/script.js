74f4cc1da49b3ac06699010d90d4166217d04c63
Benjamin Malchert <b.malchert0@gmail.com>
2021-02-09 16:05:33 -0800

    Revamp javascript to include variable sizing
    Added a reset button, deleteSketchTable and createSketchTable functions.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,56 @@
 const container = document.querySelector('#container');
-console.log(container);
 
-// Create 16 `div` squares
-for (let i = 0; i < 16; i++) {
-  const square = document.createElement('div');
-  square.style.width  = '98px';
-  square.style.height = '98px';
-  square.textContent  = 'Square ' + i;
-  square.className = 'square'
-  square.id = 'square' + i;
-  container.appendChild(square);
+// Setup the initial sketch table
+let dimension = 4;
+const CONTAINERSIZE = 600;
+container.style.height = CONTAINERSIZE + 'px';
+container.style.width  = CONTAINERSIZE + 'px';
+createSketchTable(dimension);
+
+// add reset button event listener
+const resetButton = document.querySelector('button');
+resetButton.addEventListener('click', reset);
+
+function changeColor(e) {
+  const square = document.querySelector(`#${e.target.id}`);
+  square.className += ' drawn';
+}
+
+// create reset button event handler
+function reset() {
+  const squares = Array.from(document.querySelectorAll(".square.drawn"));
+  for (let i = 0; i < squares.length; i++) {
+    squares[i].className = 'square';
+  }
+
+  do {
+    dimension = prompt("Enter dimension", dimension);
+  } while (dimension > 100);
+  deleteSketchTable();
+  createSketchTable(dimension)
+}
+
+function createSketchTable(dimension) {
+  for (let i = 0; i < dimension*dimension; i++) {
+    const square = document.createElement('div');
+
+    // create the dimension string
+    let dimensionString = CONTAINERSIZE / dimension - 2;
+    dimensionString += 'px';
+
+    square.style.width  = dimensionString;
+    square.style.height = dimensionString;  
+    square.className = 'square'
+    square.id = 'square' + i;
+    square.addEventListener('mouseover', changeColor);
+  
+    container.appendChild(square);
+  }
+}
+
+function deleteSketchTable() {
+  const squares = Array.from(document.querySelectorAll(".square"));
+  for (let i = 0; i < squares.length; i++) {
+    container.removeChild(squares[i]);
+  }
 }
